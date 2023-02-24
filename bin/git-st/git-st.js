4d90aab9bbf53000537f8ee9f8c76f2aec60846c
蓝胖子007 <1829390613@qq.com>
2023-02-24 11:19:53 +0800

    feat: feat: 增加git st sel

--- bin/git-st/git-st.js
+++ bin/git-st/git-st.js
@@ -1,9 +1,19 @@
-// console.log('hi world')
 const inquirer = require('inquirer');
 require('./utils/index')
-const child_process= require("child_process")
+const { program } = require('commander');
+const child_process= require("child_process");
 
-async function main(){
+
+program.description('stash 当前的代码').action(stachSave)
+program.command('select').alias('sel').description('查看并选择stash 代码').action(stashSelect)
+
+program.parse(process.argv)
+
+function execResultToString(result){
+  return Buffer.isBuffer(result) ? result.toString() : typeof(result) === 'string' ? result : ''
+}
+
+async function stachSave(){
   const reasonAnswer = await inquirer.prompt([{
     type:'input',
     name:'reason',
@@ -26,6 +36,50 @@ const result = child_process.execSync(`git stash save "${stashMsg}"`)
 console.log(Buffer.isBuffer(result) ? result.toString() : typeof(result) === 'string' ? result : '')
 }
 
-main()
+
+async function stashSelect() {
+  const result = child_process.execSync(`git stash list`)
+  const listStr = Buffer.isBuffer(result) ? result.toString() : typeof(result) === 'string' ? result : ''
+  const list = listStr.split('\n')
+  list.pop()
+  if(list.length === 0) {
+    console.log('stash中空空如也')
+    return
+  }
+  const {selectedStash} = await inquirer.prompt([{
+    type:'list',
+    name: 'selectedStash',
+    message: '请选择要应用的Stash',
+    choices: list
+  }])
+  const {selectedOption} = await inquirer.prompt([{
+    type:'list',
+    name: 'selectedOption',
+    message: `你选择的是${selectedStash},请选择要进行的操作`,
+    choices: ['pop','apply']
+  }])
+  const stashStr = selectedStash.split(':')[0]
+  const startIndex = stashStr.indexOf('{') + 1
+  const endIndex = stashStr.indexOf('}')
+  const index = parseInt(stashStr.substring(startIndex, endIndex))
+  switch (selectedOption) {
+    case 'pop' :
+      try {
+        const popResult = child_process.execSync(`git stash pop ${index}`)
+        console.log(execResultToString(popResult))
+      } catch (error) {
+      }
+      break
+    case 'apply' :
+      try {
+        const applyResult = child_process.execSync(`git stash apply ${index}`)
+        console.log(execResultToString(applyResult))
+      } catch (error) {
+      }
+      break
+  }
+}
+
+// stachSave()
 
 
